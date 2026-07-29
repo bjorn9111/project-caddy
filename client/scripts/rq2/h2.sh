e447e5587148163e5baccc514536eeaf5a8eb3be
Ubuntu <bjorn9111@gmail.com>
2026-07-29 14:35:38 +0000

    rq2 benchmarks

--- client/scripts/rq2/h2.sh
+++ client/scripts/rq2/h2.sh
@@ -3,14 +3,14 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$SCRIPT_DIR/common.sh"
 
-SERVER="https://10.1.0.220:8443/"
+SERVER="https://10.1.0.116:8443/"
 # SERVER="https://127.0.0.1:8443/"
 
-warmup "$SERVER" 10 --npn-list h2
-benchmark "$SERVER" "rq2/h2/low" 10000 10 --npn-list h2 -m 10
+warmup "$SERVER" 4 --npn-list h2
+benchmark "$SERVER" "rq2/h2/low" 50000 4 --npn-list h2 -m 10
 
-warmup "$SERVER" 50 --npn-list h2
-benchmark "$SERVER" "rq2/h2/medium" 10000 50 --npn-list h2 -m 10
+warmup "$SERVER" 32 --npn-list h2
+benchmark "$SERVER" "rq2/h2/medium" 50000 32 --npn-list h2 -m 10
 
-warmup "$SERVER" 100 --npn-list h2
-benchmark "$SERVER" "rq2/h2/high" 10000 100 --npn-list h2 -m 10
+warmup "$SERVER" 128 --npn-list h2
+benchmark "$SERVER" "rq2/h2/high" 50000 128 --npn-list h2 -m 10
